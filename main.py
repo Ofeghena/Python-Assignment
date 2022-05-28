244c49ccaa34b882cd03d5528fb3e5342dba1850
Ofeghena <105239094+Ofeghena@users.noreply.github.com>
2022-05-28 12:57:44 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-# Read text from a file, and count the occurence of words in that text
-# Example:
-# count_words("The cake is done. It is a big cake!") 
-# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks, score):
+        self.name = str(name)
+        self.age = int(age)
+        self.tracks = list(tracks)
+        self.score = float(score)
+        print("Details:")
+        print("Name: ", name)
+        print("Age: ", age)
+        print("Tracks: ", tracks)
+        print("Score: ", score)
+    def info(self):
+        pass
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
 
-
-
-
-
-def read_file_content(story):
-    # Solution to assignment!
-        story = open("story.txt","r")
-        contents = story.read()
-        
-        return contents
-print(read_file_content('story'))
-
-def count_words():
-    text = read_file_content("./story.txt")
-    # Solution to assignment!
-    word = text.split()
-    count = {}
-    for i in word:
-        if i in count:
-            count[i] += 1
-        else:
-            count[i] = 1
-    return count
-print(count_words())
+class Bob:
+    def __init__(self, change_name, change_age, add_track, get_score):
+        self.change_name = change_name
+        self.change_age = change_age
+        self.add_track = add_track
+        self.get_score = get_score
+        print("Details:")
+        print("change_name: ", change_name)
+        print("change_age: ", change_age)
+        print("add_track: ", add_track)
+        print("get_score:", get_score)
+        pass
+Peter = Bob(change_name="Peter", change_age="34", add_track=["FE","BE"] +["UI/UX"], get_score=20.90)
