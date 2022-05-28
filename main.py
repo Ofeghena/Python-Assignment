5120b579a58c0cafde3d20cca1d6de293d06b8ee
Ofeghena <105239094+Ofeghena@users.noreply.github.com>
2022-05-28 02:05:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,11 @@
 
 def find_anagram(str1, str2):
     # Solution to assignment!
-    str1 = "ink"
-    str2 = "kin"
+    str1 = input("Enter your first word:")
+    str2 = input("Enter your second word:"
 
     if (sorted(str1) == sorted(str2)):    
       return True
     else:
       return False
-print(find_anagram("ink","kin"))      
+print(find_anagram("str1","str2"))      
